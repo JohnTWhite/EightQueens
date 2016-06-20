5303029fd936ff46677e7791e71e8a13518fb044
John White <Captain@x-wing.local>
2016-06-19 19:55:28 -0500

    new button

--- EightQueens/ViewController.swift
+++ EightQueens/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func clear(sender: UIButton) {
+        let orginal_text =  "I was first made aware of the eight queen problem by the Youtube channel Numberphile. https://www.youtube.com/watch?v=jPcBU0Z2Hj8 I wanted to explore this problem in computer science. Using a recursive function to check safe positions for my queens which are placed on their own row, as should be obivious as to why, and then adds any succesful eighth queen placment into a string. This string then returns into another string that collects all correct positions. That result then displays in this box. "
+        ResultsOut.text = orginal_text
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
